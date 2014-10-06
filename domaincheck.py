18aa9a1c83f1c8253bbcf1aa2edf7b858149761c
Kevin Kutzko <k@kevinkevin.com>
2014-10-06 18:42:39 -0400

    changed script to do hostname ip extraction and ip lookup in one loop

--- domaincheck.py
+++ domaincheck.py
@@ -10,19 +10,25 @@ from ipwhois import IPWhois
 from pprint import pprint
 
 file = open( 'output.txt', 'w')
+file2 = open( 'output2.txt', 'w')
 with open( 'input.txt' ) as f:
     for line in f:
-         obj = IPWhois(line.strip())
+         parsed_uri = urlparse(line.strip())
+         domain = '{uri.netloc}'.format(uri=parsed_uri)
+         addr = socket.gethostbyname(domain)
+         obj = IPWhois(addr.strip())
          results = obj.lookup()
          encoded_str = json.dumps( results )
          decoded_data = json.loads( encoded_str )
-         name = decoded_data['nets'][0]['name']
-         desc = decoded_data["nets"][0]["description"]
-         emad = decoded_data["nets"][0]["abuse_emails"]
-         print 'name : ' + name + ' description : ' + desc + ' abuse email : ' + emad
+         name = decoded_data['nets'][0]['name'].replace('\n', '')
+         desc = decoded_data["nets"][0]["description"].replace('\n', '')
+         emad = decoded_data["nets"][0]["abuse_emails"].replace('\n', ' ')
+         print 'domain : ' + domain + ' name : ' + name + ' description : ' + desc + ' abuse email : ' + emad
          file.write(name + ',' + desc + ',' + emad + '\n')
+         file2.write(addr + '\n')
 
 file.close()
+file2.close()
 
 
 
